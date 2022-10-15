cfffc2d6f1f5e2114df893c179f0f1eb9ead3d7b
Mohraiel Matta <Mohraielmatta@gmail.com>
2022-10-15 16:38:19 -0700

    added resizable array stack

--- project-head/src/main/java/proj2/ResizableArrayStack.java
+++ project-head/src/main/java/proj2/ResizableArrayStack.java
@@ -1,5 +1,70 @@
 package proj2;
+import java.util.EmptyStackException;
 
-public class ResizableArrayStack {
-    
+public final class ResizableArrayStack <T> implements StackInterface<T> {
+    /**
+    A class of stacks whose entries are stored in an array.
+    @author Frank M. Carrano and Timothy M. Henry
+    @version 5.0
+*/
+	private T[] stack;    // Array of stack entries
+	private int topIndex; // Index of top entry
+   private boolean integrityOK = false;
+	private static final int DEFAULT_CAPACITY = 50;
+	private static final int MAX_CAPACITY = 10000;
+  
+   public ResizableArrayStack()
+   {
+      this(DEFAULT_CAPACITY);
+   } // end default constructor
+  
+   public ResizableArrayStack(int initialCapacity)
+   {
+      integrityOK = false;
+      checkCapacity(initialCapacity);
+      
+      // The cast is safe because the new array contains null entries
+      @SuppressWarnings("unchecked")
+      T[] tempStack = (T[])new Object[initialCapacity];
+      stack = tempStack;
+		topIndex = -1;
+      integrityOK = true;
+  } // end constructor
+  
+private void checkCapacity(int initialCapacity) {
 }
+
+//  < Implementations of the stack operations go here. >
+//  < Implementations of the private methods go here; checkCapacity and checkIntegrity
+//    are analogous to those in Chapter 2. >
+//  . . .
+@Override
+   public void push(T newEntry) {
+      // TODO Auto-generated method stub
+      
+   }
+
+   @Override
+   public T pop() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public T peek() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public boolean isEmpty() {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   @Override
+   public void clear() {
+      // TODO Auto-generated method stub
+      
+   }
+} // end ArrayStack
