a3db892e843ef2aa73632d1057d3a5a4fea7bbbc
tiff178 <tiffmt817@gmail.com>
2022-10-16 16:05:02 -0700

    test cases for ResizableArrayStackTest.java

--- project-head/src/test/java/proj2/ResizableArrayStackTest.java
+++ project-head/src/test/java/proj2/ResizableArrayStackTest.java
@@ -1,31 +1,38 @@
 package proj2;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ResizableArrayStackTest {
-	private int topIndex = -1; // Index of top entry
-    private boolean integrityOK = false;
-	private static final int DEFAULT_CAPACITY = 50;
-	private static final int MAX_CAPACITY = 10000;
-
-  // end constructor
+    /** Test case values: push 5,4,3,2,1 and pop twice
+ *   Expected output: value at the top is 3 */
     @Test
-    public void isEmpty() {
-        if (topIndex < 0) {
-            System.out.println("Stack is empty");
-        }
+    <T> void checkStack() {
+        ResizableArrayStack<Integer> st = new ResizableArrayStack<>();
+        st.push(5); 
+        st.push(4);
+        st.push(3);
+        st.push(2);
+        st.push(1);
+        st.pop();
+        st.pop();
+        assertEquals(3, st.pop());
     }
 
-    public void pop() {
-
-
+    /** Test case values: push 5,4,3,2,1,0
+ *   Expected output: stack is not empty */
+    @Test
+    <T> void stackNotEmpty() {
+        ResizableArrayStack<Integer> st = new ResizableArrayStack<>();
+        st.push(5); 
+        st.push(4);
+        st.push(3);
+        st.push(2);
+        st.push(1);
+        st.push(0);
+        st.pop();
+        st.pop();
+        assertEquals(false, st.isEmpty());
     }
-
-    public void peek() {
-
-    }
-
-
 
 }
 
