4ce121c65ac3b0a6fcf00629321504e295e4f55b
tiff178 <tiffmt817@gmail.com>
2022-10-16 14:28:55 -0700

    test cases added for LinkedStackTest

--- project-head/src/test/java/proj2/LinkedStackTest.java
+++ project-head/src/test/java/proj2/LinkedStackTest.java
@@ -1,18 +1,37 @@
 package proj2;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Stack;
-
 public class LinkedStackTest {
-    private Stack testStack;
-
-	@Test 
-    public void checkifNull ()
-    {
-        testStack.push(null);
-        assertEquals(null, testStack.pop());
+    /** Test case values: push 5,4,3,2,1 and pop twice
+ *   Expected output: value at the top is 3 */
+    @Test
+    <T> void checkStack() {
+        LinkedStack<Integer> st = new LinkedStack<>();
+        st.push(5); 
+        st.push(4);
+        st.push(3);
+        st.push(2);
+        st.push(1);
+        st.pop();
+        st.pop();
+        assertEquals(3, st.pop());
     }
-      
+
+    /** Test case values: push 5,4,3,2,1,0
+ *   Expected output: stack is not empty */
+    @Test
+    <T> void stackNotEmpty() {
+        LinkedStack<Integer> st = new LinkedStack<>();
+        st.push(5); 
+        st.push(4);
+        st.push(3);
+        st.push(2);
+        st.push(1);
+        st.push(0);
+        st.pop();
+        st.pop();
+        assertEquals(false, st.isEmpty());
+    }
+
 }
