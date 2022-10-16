8088b38d959fb715d8f9f6041e8cc2ed5727bde1
Mohraiel Matta <Mohraielmatta@gmail.com>
2022-10-15 18:31:26 -0700

    sorry idk what happened, should fix now

--- project-head/src/main/java/proj2/ResizableArrayStack.java
+++ project-head/src/main/java/proj2/ResizableArrayStack.java
@@ -1,8 +1,9 @@
 package proj2;
+import java.util.Arrays;
 import java.util.EmptyStackException;
 
 public final class ResizableArrayStack <T> implements StackInterface<T> {
-    /**
+ /**
     A class of stacks whose entries are stored in an array.
     @author Frank M. Carrano and Timothy M. Henry
     @version 5.0
@@ -20,9 +21,7 @@ public final class ResizableArrayStack <T> implements StackInterface<T> {
   
    public ResizableArrayStack(int initialCapacity)
    {
-      integrityOK = false;
       checkCapacity(initialCapacity);
-      
       // The cast is safe because the new array contains null entries
       @SuppressWarnings("unchecked")
       T[] tempStack = (T[])new Object[initialCapacity];
@@ -31,40 +30,112 @@ public final class ResizableArrayStack <T> implements StackInterface<T> {
       integrityOK = true;
   } // end constructor
   
-private void checkCapacity(int initialCapacity) {
-}
 
-//  < Implementations of the stack operations go here. >
-//  < Implementations of the private methods go here; checkCapacity and checkIntegrity
+   /** 
+   * Adds a value to the top of the stack.
+   * @param newEntry A New value is placed on the stack.
+   */
+   public void push(T newEntry)
+   {
+      checkIntegrity();
+      ensureCapacity();
+      stack[topIndex + 1] = newEntry;
+      topIndex++;
+   } // end push
+
+   
+   /** 
+    * Removes a value from the top of the stack.
+    * @return top, The top value of the stack.
+    */
+   public T pop()
+   {
+      checkIntegrity();
+      if (isEmpty()){
+         throw new EmptyStackException();
+      }
+      else
+      {
+         T top = stack[topIndex];
+         stack[topIndex] = null;
+         topIndex--;
+         return top;
+      } // end if
+   } // end pop
+
+   
+   /** 
+    * Looks at the top value of the stack.
+    * @return T, top value of the stack.
+    * @throws EmptyStackException() if stack is empty.
+    */
+   public T peek()
+   {
+      checkIntegrity();
+      if (isEmpty()){
+         throw new EmptyStackException();
+      }
+      else{
+         return stack[topIndex];
+      }
+   } // end peek
+
+   
+   /** 
+    * Determines if stack is empty or not.
+    * @return isEmpty(), true if stack is empty.
+    */
+   public boolean isEmpty()
+   {
+      return topIndex < 0;
+   } // end isEmpty
+
+   /** 
+    * Removes all values from a stack.
+    */
+   public void clear(){
+      checkIntegrity();
+
+      //Remove references to the objects in the stack,
+      //but do not deallocate the array
+      while (topIndex > -1){
+         stack[topIndex] = null;
+         topIndex--;
+      }//end while
+      //Assertion: topIndex is -1
+   }//end clear
+
+   /** 
+    * Resizes stack if needed.
+    */
+   private void ensureCapacity(){
+      if (topIndex >= stack.length - 1)//If array is full, double its size
+      {
+         int newLength = 2 * stack.length;
+         checkCapacity(newLength);
+         stack = Arrays.copyOf(stack, newLength);
+      }//end if
+   }//end ensureCapacity
+
+   
+   /** 
+    * Checks if new resized capacity exceeds allowed capacity.
+    * @param capacity   Desired capacity when ensureCapacity tries to resize.
+      @throws IllegalStateException() if capacity exceeds allowed capacity.
+    */
+   private void checkCapacity(int capacity)
+      {
+         if (capacity > MAX_CAPACITY)
+            throw new IllegalStateException("Attempt to create a bag whose " +
+                                            "capacity exeeds allowed " +
+                                            "maximum of " + MAX_CAPACITY);
+      } // end checkCapacity
+
+   private void checkIntegrity()
+      {
+         if (!integrityOK)
+            throw new SecurityException("ResizableArrayStack object is corrupt.");
+      } // end checkIntegrity
 //    are analogous to those in Chapter 2. >
 //  . . .
-@Override
-   public void push(T newEntry) {
-      // TODO Auto-generated method stub
-      
-   }
-
-   @Override
-   public T pop() {
-      // TODO Auto-generated method stub
-      return null;
-   }
-
-   @Override
-   public T peek() {
-      // TODO Auto-generated method stub
-      return null;
-   }
-
-   @Override
-   public boolean isEmpty() {
-      // TODO Auto-generated method stub
-      return false;
-   }
-
-   @Override
-   public void clear() {
-      // TODO Auto-generated method stub
-      
-   }
 } // end ArrayStack
