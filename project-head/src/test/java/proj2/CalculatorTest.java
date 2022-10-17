853c3ff7cae599e97617fe75e5e45cd024176a23
Mohraiel Matta <Mohraielmatta@gmail.com>
2022-10-17 16:19:01 -0700

    added three more detailed tests with evaluation

--- project-head/src/test/java/proj2/CalculatorTest.java
+++ project-head/src/test/java/proj2/CalculatorTest.java
@@ -51,4 +51,30 @@ public class CalculatorTest {
         assertEquals(emp, "");
     }
 
-}
+    @Test
+    void anotherexcalc(){
+            String infix = "a+b*(c/a)+d*e";
+            System.out.println("Infix expression: " + infix + "\nPostfix expression: " + Calculator.convertToPostfix(infix));
+    
+            int result = Calculator.evaluatePostfix(Calculator.convertToPostfix(infix));
+            System.out.println("Assuming when a=2, b=3, c=4, d=5, e=6, the result is: " + result);
+        }
+    
+     @Test
+        void moreparenthesis(){
+                String infix = "a+b*(c/a)/(d-e)";
+                System.out.println("Infix expression: " + infix + "\nPostfix expression: " + Calculator.convertToPostfix(infix));
+        
+                int result = Calculator.evaluatePostfix(Calculator.convertToPostfix(infix));
+                System.out.println("Assuming when a=2, b=3, c=4, d=5, e=6, the result is: " + result);
+            }
+    @Test
+        void lessletters(){
+                String infix = "(a*c)+(e+b)";
+                System.out.println("Infix expression: " + infix + "\nPostfix expression: " + Calculator.convertToPostfix(infix));
+            
+                int result = Calculator.evaluatePostfix(Calculator.convertToPostfix(infix));
+                System.out.println("Assuming when a=2, b=3, c=4, d=5, e=6, the result is: " + result);
+                }   
+        
+    }
